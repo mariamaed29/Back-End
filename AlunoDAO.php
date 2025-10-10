60c0bde912f8374f6c41a4c13a6d944f10fa552a
mariamaed29 <maed.eduarda2009@gmail.com>
2025-10-10 16:55:33 -0300

    Add files via upload

---
+++ AlunoDAO.php
@@ -0,0 +1,15 @@
+<?php
+
+class AlunoDAO {
+    private $alunos = [];
+    public function criarAluno(){
+    }
+    public function lerAluno(){
+    }
+
+    public function atualizarAluno(){
+    }
+
+    public function excluirAluno(){
+    }
+}
